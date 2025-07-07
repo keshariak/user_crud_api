528c0325214c1e0b201fb5a988290340aaa3125d
keshariak <aryankeshari2003@gmail.com>
2025-07-07 17:03:02 +0530

    ok

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const User = require("../models/User");
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find().sort({ createdAt: -1 });
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
+
+// Add user
+exports.addUser = async (req, res) => {
+  try {
+    const { name, email, mobile, age } = req.body;
+    const newUser = new User({ name, email, mobile, age });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// Update user
+exports.updateUser = async (req, res) => {
+  try {
+    const updated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: "Invalid data" });
+  }
+};
+
+// Delete user
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    res.status(400).json({ error: "Delete failed" });
+  }
+};
