7836f48c8492f6a8551cd220aeade80bed2a259f
keshariak <aryankeshari2003@gmail.com>
2025-07-07 18:06:13 +0530

    add /

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://user-crud-ui.vercel.app"]
+  origin: ["http://localhost:3000", "https://user-crud-ui.vercel.app/"]
 }));
 
 
